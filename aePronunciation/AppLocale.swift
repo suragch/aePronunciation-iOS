c0c7172656c4dbf20eeb45c97c35f8c8d4e22755
suragch <studymongolian@gmail.com>
2020-05-24 13:36:30 +0800

    version 2.2.0

--- aePronunciation/AppLocale.swift
+++ aePronunciation/AppLocale.swift
@@ -11,20 +11,29 @@ class AppLocale {
             return Locale(identifier: "zh_Hans") // Simplified Chinese
         case "zh_Hant":
             return Locale(identifier: "zh_Hant") // Traditional Chinese
+        case "ru":
+            return Locale(identifier: "ru") // Russian
+        case "ja":
+            return Locale(identifier: "ja") // Japanese
+        case "mn":
+            return Locale(identifier: "mn") // Mongolian
+        case "vi":
+            return Locale(identifier: "vi") // Vietnamese
         default:
             return Locale(identifier: "en_US_POSIX") // US English
         }
     }
     
     class func getFormattedDate(date: Date) -> String {
-        //let formatter = DateFormatter()
-        //formatter.locale = getLocale()
-        //formatter.dateStyle = DateFormatter.Style.long
-        //return formatter.string(from: date)
-        
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d H:mm"
+        formatter.locale = getLocale()
+        formatter.dateStyle = DateFormatter.Style.medium
+        formatter.timeStyle = DateFormatter.Style.medium
         return formatter.string(from: date)
+        
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy/M/d H:mm"
+//        return formatter.string(from: date)
     }
 
 }
